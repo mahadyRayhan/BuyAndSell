6648830a4bbcdc216c48345f9440b22a08e8c3d9
rayhan <rayhan.feluda@gmail.com>
2017-05-25 11:17:43 +0600

    first

---
+++ app/src/main/java/com/example/nocturnal/buyandsell/Activity/LogInActivity.java
@@ -0,0 +1,15 @@
+package com.example.nocturnal.buyandsell.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.nocturnal.buyandsell.R;
+
+public class LogInActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
